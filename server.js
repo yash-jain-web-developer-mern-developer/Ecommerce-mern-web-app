2dec5eeaff1e95f2a2137c4883165a0c58ac0b60
Yash Jain <105738560+yashjaincodder@users.noreply.github.com>
2024-04-17 00:06:06 +0530

    make deploy ready

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors"
 import path from "path";
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
+const __dirname = path.resolve();
 
 
 //configure env
@@ -27,17 +26,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./client/build")))
+
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
+app.use(express.static(path.join(__dirname,"/client/build")))
 //rest api
-app.use('*',function(req,res){
-res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'client','build','index.html'))
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
